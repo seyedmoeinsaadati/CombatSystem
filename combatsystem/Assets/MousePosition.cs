0e424b956b13b1171a06b034aae91dd10ac9c2d4
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-06-22 18:31:11 +0330

    Add MousePosition.cs Add simple IK for head tracking

---
+++ combatsystem/Assets/MousePosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    [SerializeField] private float yPos = 1;
+    private Vector3 worldPosition;
+    private Plane plane;
+
+    private void Start()
+    {
+        plane = new Plane(Vector2.up, yPos);
+    }
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            worldPosition = ray.GetPoint(distance);
+            transform.position = worldPosition;
+        }
+    }
+}
